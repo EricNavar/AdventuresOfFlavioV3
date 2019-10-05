a4729ee7a68107245278ce95aead8667f16ac60d
Eric <ericnavar@ufl.edu>
2019-10-05 18:17:28 -0400

    Keep camera in bounds - CameraController stays in bounds when a Tilemap is passed to it in the inspector - The Camera will only stay in bounds in this given scene and will later have to be further updated to work for every scene.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    
+    public Tilemap tm;
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+    private float halfHeight;
+    private float halfWidth;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +19,19 @@ public class CameraController : MonoBehaviour
             target = PlayerController.instance.transform;
         else
             Debug.Log("player instance has not yet been created.");
+        tm.CompressBounds();
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+        Vector3 halfCameraSize = new Vector3(halfWidth, halfHeight, 0);
+        bottomLeftLimit = tm.localBounds.min + halfCameraSize;
+        topRightLimit = tm.localBounds.max - halfCameraSize;
     }
 
     // LateUpdate is called once per frame
     void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, bottomLeftLimit.x, topRightLimit.x),
+            Mathf.Clamp(target.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
     }
 
     public void setTarget(Transform t)
